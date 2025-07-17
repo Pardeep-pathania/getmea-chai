2c8ddfba431ceb07764cee29005ac2a2ce22ebc2
pardeep-pathania <pathanianikhil145@gmail.com>
2025-07-17 14:38:34 +0530

    project initailised

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <div className="flex flex-col justify-center items-center text-white h-[44vh] ">
+    <div className="text-3xl"> Buy Me a Chai</div>
+
+<p>
+  A crowdfunding platform for creators. Get funded by your fans and followers. Start Now!
+</p>
+<div><button>Start now </button> </div>
+
+ </div>
+  );
+}
