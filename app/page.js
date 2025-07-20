8d80358d1f53ab9f969e69f6761fe326425800cf
pardeep-pathania <pathanianikhil145@gmail.com>
2025-07-20 11:59:03 +0530

    home page and next auth integration complete

--- app/page.js
+++ app/page.js
@@ -1,15 +1,76 @@
+
+"use client"
 import Image from "next/image";
 
 export default function Home() {
   return (
-  <div className="flex flex-col justify-center items-center text-white h-[44vh] ">
-    <div className="text-3xl"> Buy Me a Chai</div>
+    
+    <>
+    
+      <div className="flex flex-col justify-center items-center text-white h-[44vh] ">
+        <div className="text-5xl font-bold flex justify-center items-center">
+          {" "}
+          Buy Me a Chai{" "}
+          <span>
+            <img src="/tea.gif" width={90} alt="" />
+          </span>{" "}
+        </div>
 
-<p>
-  A crowdfunding platform for creators. Get funded by your fans and followers. Start Now!
-</p>
-<div><button>Start now </button> </div>
+        <p>
+          A crowdfunding platform for creators. Get funded by your fans and
+          followers. Start Now!
+        </p>
+        <div>
+          <button
+            type="button"
+            className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          >
+            Start Now
+          </button>
 
- </div>
+          <button
+            type="button"
+            className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          >
+            Read more
+          </button>
+        </div>
+      </div>
+
+      <div className="bg-white h-1 opacity-10"></div>
+
+      <div className="text-white container mx-auto py-16">
+        <h1 className="text-3xl font-bold text-center mb-10">
+          Your Fans can buy you a chai
+        </h1>
+        <div className="flex gap-5 justify-around">
+          <div className="item space-y-3 ">
+            <img className="bg-slate-400 rounded-full p-2 text-black" src="/man.gif" width={90} alt="" />
+           <p className="font-bold">Fans want to help</p>
+            <p className="flex-center">Your fans are available for you to help you</p>
+          </div>
+          <div className="item space-y-3">
+            <img className=" bg-slate-400 rounded-full p-2 text-black" src="/coin.gif" width={90} alt="" />
+            <p className="font-bold">Fans want to help</p>
+            <p className="flex-center">Your fans are available for you to help you</p>
+          </div>
+          <div className="item space-y-3">
+            <img className=" bg-slate-400 rounded-full p-2 text-black" src="/group.gif" width={90} alt="" />
+            <p className="font-bold">Fans want to help</p>
+            <p className="flex-center">Your fans are available for you to help you</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-white h-1 opacity-10"></div>
+
+      <div className="text-white container mx-auto pb-32 pt-14 flex flex-col items-center justify-center">
+        <h1 className="text-3xl font-bold text-center mb-10">
+          Learn  more about us
+        </h1>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/0Gtn2gqE8R0?si=mLRIde_xLQXMBo4L" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+    </>
+    
   );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,136 @@
-import React from 'react'
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const { data: session } = useSession();
+  const [showdropdown, setShowdropdown] = useState(false);
+  // if(session){
+  //   return <>
+  //   Signed in as {session.user.email} <br/>
+  //   <button onClick={() => signOut()}>Sign out</button>
+  //   </>
+  // }
   return (
-    <nav className='bg-blue-950 text-white flex justify-between items-center px-4 h-16'>
-      <div className="logo font-bold text-xl">GetMeAChai</div>
+    <nav className="bg-blue-950 text-white flex justify-between items-center px-4 h-16">
+      <Link href={"/"} className="logo font-bold text-xl flex justify-center items-center">
+        
+        <span>GetMeAChai</span>
+        <img src="/tea.gif" width={44} alt="" />
+      
+      </Link>
 
-    <ul className="flex justify-between gap-4">
+      {/* <ul className="flex justify-between gap-4">
       <li>Home</li>
       <li>About</li>
       <li>Projects</li>
       <li>Sign Up</li>
       <li>Login</li>
-    </ul>
+    </ul> */}
 
+      <div className="relative">
+        {/* dropdown menu */}
+
+        {session && (
+          <>
+            
+            <button
+              onClick={() => {setShowdropdown(!showdropdown)
+              
+              }}
+              id="dropdownDefaultButton"
+              data-dropdown-toggle="dropdown"
+              className="text-white mx-2 bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
+              type="button"
+            >
+              Welcome {session.user.email}
+              <svg
+                className="w-2.5 h-2.5 ms-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 10 6"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m1 1 4 4 4-4"
+                />
+              </svg>
+            </button>
+            <div
+              id="dropdown"
+              className={`z-10 ${
+                showdropdown ? " " : "hidden"
+              } absolute left-[130px] bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700`}
+            >
+              <ul
+                className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                aria-labelledby="dropdownDefaultButton"
+              >
+                <li>
+                  <Link
+                    href="/dashboard"
+                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Dashboard
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="#"
+                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Settings
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="#"
+                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Earnings
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                  onClick={()=>{signOut()}}
+                    href="#"
+                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Sign out
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </>
+        )}
+       
+        {session && (
+          <button
+            onClick={() => {
+              signOut();
+            }}
+            className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          >
+            Logout
+          </button>
+        )}
+
+        {!session && (
+          <Link href={"/login"}>
+            <button className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+              Login
+            </button>
+          </Link>
+        )}
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
